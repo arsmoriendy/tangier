0f00351a0420879fd9fafe728bc3992ef8516336
arsmoriendy <nugrahasteven@gmail.com>
2026-04-24 19:26:14 +0700

    feat: dynamic sidebar links

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import {
   Sidebar,
   SidebarContent,
@@ -8,8 +10,24 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
-import { MoneyIcon } from "@phosphor-icons/react/ssr"
+import { MoneyIcon, PackageIcon } from "@phosphor-icons/react/ssr"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { ReactNode } from "react"
+
+function SidebarLink(props: { children: ReactNode; href: string }) {
+  const activeClass =
+    "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground hover:brightness-90"
+  const isActive = usePathname().startsWith(props.href)
+
+  return (
+    <SidebarMenuItem>
+      <SidebarMenuButton className={isActive ? activeClass : ""} asChild>
+        <Link href={props.href}>{props.children}</Link>
+      </SidebarMenuButton>
+    </SidebarMenuItem>
+  )
+}
 
 export function AppSidebar() {
   return (
@@ -18,14 +36,14 @@ export function AppSidebar() {
       <SidebarContent>
         <SidebarGroup>
           <SidebarMenu>
-            <SidebarMenuItem>
-              <SidebarMenuButton asChild>
-                <Link href="/price-groups">
-                  <MoneyIcon />
-                  Price groups
-                </Link>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
+            <SidebarLink href="/items">
+              <PackageIcon />
+              Items
+            </SidebarLink>
+            <SidebarLink href="/price-groups">
+              <MoneyIcon />
+              Price groups
+            </SidebarLink>
           </SidebarMenu>
         </SidebarGroup>
       </SidebarContent>
