c2d2360c2275ffd5e2562d092a62aca98f727671
arsmoriendy <nugrahasteven@gmail.com>
2026-04-26 17:15:09 +0700

    fix: idr input height

--- components/ui/idr-input.tsx
+++ components/ui/idr-input.tsx
@@ -23,12 +23,12 @@ export function IdrInput({
       aria-invalid={ariaInvalid}
       className={cn(
         className,
-        "flex w-full min-w-0 rounded-none border border-input bg-transparent transition-colors outline-none focus-within:border-ring focus-within:ring-1 focus-within:ring-ring/50 aria-disabled:pointer-events-none aria-disabled:cursor-not-allowed aria-disabled:bg-input/50 aria-disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-1 aria-invalid:ring-destructive/20 md:text-xs dark:bg-input/30 dark:aria-disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40"
+        "flex h-8 w-full min-w-0 rounded-none border border-input bg-transparent transition-colors outline-none focus-within:border-ring focus-within:ring-1 focus-within:ring-ring/50 aria-disabled:pointer-events-none aria-disabled:cursor-not-allowed aria-disabled:bg-input/50 aria-disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-1 aria-invalid:ring-destructive/20 md:text-xs dark:bg-input/30 dark:aria-disabled:bg-input/80 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40"
       )}
     >
       <span
         className={cn(
-          "bg-muted p-2 text-muted-foreground select-none",
+          "grid aspect-square place-items-center bg-muted text-center text-muted-foreground select-none",
           ariaInvalid ? "bg-destructive text-background" : ""
         )}
       >
@@ -41,7 +41,7 @@ export function IdrInput({
         decimalSeparator=","
         thousandSeparator="."
         unstyled
-        className="h-8 w-full min-w-0 rounded-none px-2.5 py-1 text-xs transition-colors outline-none placeholder:text-muted-foreground md:text-xs"
+        className="w-full min-w-0 rounded-none px-2.5 py-1 text-xs transition-colors outline-none placeholder:text-muted-foreground md:text-xs"
         {...props}
       />
     </div>
