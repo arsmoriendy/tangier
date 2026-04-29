d0e2746ddb6c0fcdf57b72c200c825d2c2e509c3
arsmoriendy <nugrahasteven@gmail.com>
2026-04-29 15:48:24 +0700

    style: create price group form

--- app/price-groups/create-price-group-form.tsx
+++ app/price-groups/create-price-group-form.tsx
@@ -2,7 +2,7 @@
 
 import { getRandomColor } from "@/app/price-groups/price-group-colors"
 import { Form, useAppForm } from "@/components/form"
-import { FieldLabel } from "@/components/ui/field"
+import { FieldLabel, FieldLegend, FieldSet } from "@/components/ui/field"
 import { usePriceGroups } from "@/contexts/price-groups-ctx"
 import { createPriceGroup, listPriceGroups } from "@/lib/crud/price-group"
 import { ArrowsClockwiseIcon } from "@phosphor-icons/react/dist/ssr"
@@ -44,54 +44,56 @@ export default function CreatePriceGroupForm() {
   })
 
   return (
-    <Form handleSubmit={form.handleSubmit}>
-      <div className="flex gap-2">
-        <form.AppField
-          name="name"
-          children={(f) => <f.TextField label="Name" />}
-        />
+    <FieldSet>
+      <FieldLegend>Add price group</FieldLegend>
 
-        <form.AppField name="quantityThreshold">
-          {(f) => (
-            <f.NumberField label="Min qty" min={0} className="max-w-16" />
-          )}
-        </form.AppField>
+      <Form handleSubmit={form.handleSubmit}>
+        <div className="flex gap-2">
+          <form.AppField
+            name="name"
+            children={(f) => <f.TextField label="Name" />}
+          />
 
-        <form.AppField name="hexColor">
-          {(f) => (
-            <div className="space-y-2">
-              <FieldLabel>Color</FieldLabel>
-              <div className="flex gap-2">
-                <form.Subscribe selector={(state) => state.values.hexColor}>
-                  {(hexColor) => (
-                    <div
-                      className="group relative grid size-8 cursor-pointer place-items-center bg-black"
-                      onClick={() =>
-                        form.setFieldValue("hexColor", getRandomColor())
-                      }
-                    >
-                      <ArrowsClockwiseIcon className="z-1 hidden group-hover:block" />
+          <form.AppField name="quantityThreshold">
+            {(f) => <f.NumberField className="w-18" label="Min qty" min={0} />}
+          </form.AppField>
+
+          <form.AppField name="hexColor">
+            {(f) => (
+              <div className="space-y-2">
+                <FieldLabel>Color</FieldLabel>
+                <div className="flex gap-2">
+                  <form.Subscribe selector={(state) => state.values.hexColor}>
+                    {(hexColor) => (
                       <div
-                        className="absolute size-8 group-hover:opacity-90"
-                        style={{ backgroundColor: `#${hexColor}` }}
-                      />
-                    </div>
-                  )}
-                </form.Subscribe>
-                <f.TextField className="min-w-16" />
+                        className="group relative grid size-8 cursor-pointer place-items-center"
+                        onClick={() =>
+                          form.setFieldValue("hexColor", getRandomColor())
+                        }
+                      >
+                        <ArrowsClockwiseIcon className="z-1 hidden group-hover:block" />
+                        <div
+                          className="absolute size-8 rounded-full group-hover:opacity-90"
+                          style={{ backgroundColor: `#${hexColor}` }}
+                        />
+                      </div>
+                    )}
+                  </form.Subscribe>
+                  <f.TextField className="min-w-16" />
+                </div>
               </div>
-            </div>
-          )}
-        </form.AppField>
+            )}
+          </form.AppField>
+        </div>
 
         <form.AppField name="description">
           {(f) => <f.TextField label="Description" />}
         </form.AppField>
-      </div>
 
-      <form.AppForm>
-        <form.SubmitButton>Create price group</form.SubmitButton>
-      </form.AppForm>
-    </Form>
+        <form.AppForm>
+          <form.SubmitButton>Create price group</form.SubmitButton>
+        </form.AppForm>
+      </Form>
+    </FieldSet>
   )
 }
