b0a7628c896b836dc4a4fa6927320a49c054e731
arsmoriendy <nugrahasteven@gmail.com>
2026-05-01 07:52:06 +0700

    feat: admin and active auth column

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,29 @@
 import { betterAuth } from "better-auth"
 import { drizzleAdapter } from "better-auth/adapters/drizzle"
 import { db } from "@/lib/db"
+import { admin } from "better-auth/plugins"
+import { countUser } from "@/lib/crud/users"
 
 export const auth = betterAuth({
   emailAndPassword: { enabled: true },
+  user: {
+    additionalFields: {
+      active: { type: "boolean", defaultValue: false, input: false },
+    },
+  },
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
+  databaseHooks: {
+    user: {
+      create: {
+        // administrate and activate first user
+        before: async (user) => {
+          const userCount = await countUser()
+          return { data: { ...user, active: userCount === 0, role: "admin" } }
+        },
+      },
+    },
+  },
+  plugins: [admin()],
 })
