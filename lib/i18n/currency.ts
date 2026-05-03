d16c8f396ad6fab854f3d89b6a2b8607166b7ab7
arsmoriendy <nugrahasteven@gmail.com>
2026-05-03 21:52:38 +0700

    fix: consistent currency format

--- lib/i18n/currency.ts
+++ lib/i18n/currency.ts
@@ -1,6 +1,8 @@
 export const currencyFormatter = new Intl.NumberFormat("id-ID", {
   style: "currency",
   currency: "IDR",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
 })
 
 export function formatCurrency(x: number) {
