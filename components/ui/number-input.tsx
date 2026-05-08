3011efe916a9cd9b68e73d92cd3daac8f6e13bc4
arsmoriendy <nugrahasteven@gmail.com>
2026-05-08 11:00:07 +0700

    refactor: propagate number input's state to caller

--- components/ui/number-input.tsx
+++ components/ui/number-input.tsx
@@ -1,6 +1,6 @@
 import { inputClass } from "@/components/ui/input"
 import { cn } from "@/lib/utils"
-import { useEffect, useRef, useState } from "react"
+import { useRef } from "react"
 import { NumericFormat, NumericFormatProps } from "react-number-format"
 
 export type NumberInputProps = Omit<
@@ -12,12 +12,11 @@ export type NumberInputProps = Omit<
   min?: number
   max?: number
   unstyled?: true
-  value?: number
-  onValueChange?: (value: number) => any
+  value: number
+  onValueChange: (value: number) => any
 }
 
 export function NumberInput({
-  defaultValue = 0,
   step = 1,
   min = -Infinity,
   max = Infinity,
@@ -29,43 +28,35 @@ export function NumberInput({
   value: valueProp,
   ...props
 }: NumberInputProps) {
-  const [shadowValue, setShadowValue] = useState(valueProp ?? defaultValue)
   const clamp = (v: number): number => Math.min(max, Math.max(min, v))
   const inputRef = useRef<HTMLInputElement>(null)
-
-  useEffect(() => {
-    if (valueProp !== undefined && valueProp === shadowValue) return
-    onValueChange?.(shadowValue)
-  }, [shadowValue])
 
   return (
     <NumericFormat
       getInputRef={inputRef}
-      value={valueProp ?? shadowValue}
+      value={valueProp}
       className={!unstyled ? cn(inputClass, className) : className}
       min={min}
       max={max}
       onValueChange={({ floatValue }) => {
-        if (
-          floatValue !== undefined &&
-          (valueProp ?? shadowValue) !== floatValue
-        )
-          setShadowValue(floatValue)
+        if (floatValue !== undefined) onValueChange(floatValue)
       }}
       onKeyDown={(e) => {
         if (e.key == "ArrowUp") {
           e.preventDefault()
-          setShadowValue((old) => clamp(old + step))
+          onValueChange(clamp(valueProp + step))
         }
         if (e.key == "ArrowDown") {
           e.preventDefault()
-          setShadowValue((old) => clamp(old - step))
+          onValueChange(clamp(valueProp - step))
         }
         onKeyDown?.(e)
       }}
       onWheel={(e) => {
         if (document.activeElement === inputRef.current) {
-          setShadowValue((old) => clamp(e.deltaY < 0 ? old + step : old - step))
+          onValueChange(
+            clamp(e.deltaY < 0 ? valueProp + step : valueProp - step)
+          )
         }
         onWheel?.(e)
       }}
