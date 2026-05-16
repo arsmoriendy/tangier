6d80bbbf4145b4611430e0bdf0031c56ffdbdaac
arsmoriendy <nugrahasteven@gmail.com>
2026-05-16 13:53:36 +0700

    chore: internal usb package

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   optimizePackageImports: ["@phosphor-icons/react"],
-  serverExternalPackages: ["usb"],
+  // serverExternalPackages: ["usb"],
   output: "standalone",
 }
 
