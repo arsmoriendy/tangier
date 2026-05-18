a53dea71f0de9a77f4a2b2bce3b60b46ca89bd54
arsmoriendy <nugrahasteven@gmail.com>
2026-05-18 16:32:54 +0700

    refactor: remove item page client

--- app/(authorized)/(admin)/items/page.tsx
+++ app/(authorized)/(admin)/items/page.tsx
@@ -2,20 +2,41 @@ import { listBarcodeGroups } from "@/lib/crud/barcode-groups"
 import { listItems } from "@/lib/crud/items"
 import { listPriceGroups } from "@/lib/crud/price-groups"
 import { listUnits } from "@/lib/crud/units"
-import ItemsPageClient from "./page-client"
+import { BarcodeGroupsProvider } from "@/contexts/barcode-groups-ctx"
+import { PriceGroupsProvider } from "@/contexts/price-groups-ctx"
+import { UnitsProvider } from "@/contexts/units-ctx"
+import { FieldLegend, FieldSet } from "@/components/ui/field"
+import ItemForm from "./item-form"
+import { ItemsProvider } from "@/contexts/items-ctx"
+import { ItemFiltersProvider } from "./item-filters-ctx"
+import { ItemCountProvider } from "./item-count-ctx"
+import ItemList from "./item-list"
+import { getTranslations } from "next-intl/server"
 
 export default async function Page() {
   const priceGroups = await listPriceGroups()
   const barcodeGroups = await listBarcodeGroups()
   const items = await listItems()
   const units = await listUnits()
+  const t = await getTranslations("items")
 
   return (
-    <ItemsPageClient
-      priceGroups={priceGroups}
-      barcodeGroups={barcodeGroups}
-      items={items}
-      units={units}
-    />
+    <BarcodeGroupsProvider barcodeGroups={barcodeGroups}>
+      <PriceGroupsProvider priceGroups={priceGroups}>
+        <UnitsProvider units={units}>
+          <FieldSet>
+            <FieldLegend>{t("createNew")}</FieldLegend>
+            <ItemForm />
+          </FieldSet>
+          <ItemsProvider items={items}>
+            <ItemFiltersProvider initialValue={{}}>
+              <ItemCountProvider>
+                <ItemList />
+              </ItemCountProvider>
+            </ItemFiltersProvider>
+          </ItemsProvider>
+        </UnitsProvider>
+      </PriceGroupsProvider>
+    </BarcodeGroupsProvider>
   )
 }
