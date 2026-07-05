2ba5622b03e727bfb1dfa53d1b2d5f476612df4a
arsmoriendy <nugrahasteven@gmail.com>
2026-07-05 16:32:05 +0700

    style: remove field legend's bottom margin

--- components/ui/field.tsx
+++ components/ui/field.tsx
@@ -30,7 +30,7 @@ function FieldLegend({
       data-slot="field-legend"
       data-variant={variant}
       className={cn(
-        "mb-2.5 font-medium data-[variant=label]:text-xs data-[variant=legend]:text-sm",
+        "font-medium data-[variant=label]:text-xs data-[variant=legend]:text-sm",
         className
       )}
       {...props}
