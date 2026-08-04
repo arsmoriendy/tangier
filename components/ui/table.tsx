fbd2975f0ed72be33472496f2f8d373692a17da4
arsmoriendy <nugrahasteven@gmail.com>
2026-08-04 11:28:50 +0700

    style: smaller td pad in smaller screens

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -78,7 +78,8 @@ function TableCell({ className, ...props }: React.ComponentProps<"td">) {
     <td
       data-slot="table-cell"
       className={cn(
-        "p-2 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0",
+        "p-1 align-middle whitespace-nowrap xl:p-2 [&:has([role=checkbox])]:pr-0",
+
         className
       )}
       {...props}
