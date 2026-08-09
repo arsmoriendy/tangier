4ef22eccce697c0f40cb560ffe5514a4c1a54715
arsmoriendy <nugrahasteven@gmail.com>
2026-08-09 15:56:37 +0700

    style: dynamic table header height

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -65,7 +65,7 @@ function TableHead({ className, ...props }: React.ComponentProps<"th">) {
     <th
       data-slot="table-head"
       className={cn(
-        "h-10 px-2 text-left align-middle font-medium whitespace-nowrap text-foreground [&:has([role=checkbox])]:pr-0",
+        "h-8 px-2 text-left align-middle font-medium whitespace-nowrap text-foreground xl:h-10 [&:has([role=checkbox])]:pr-0",
         className
       )}
       {...props}
